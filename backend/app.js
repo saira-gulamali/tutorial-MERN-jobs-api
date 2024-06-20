42311df33693c029c6c052adebb0b2a56bb93714
saira-gulamali <sguacamole98@gmail.com>
2024-06-20 16:33:09 +0100

    add backend security packages, add api documentation

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,11 @@ require("dotenv").config();
 require("express-async-errors");
 const express = require("express");
 const app = express();
+// extra security packages
+const helmet = require("helmet");
 const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
 
 // connectDB
 const connectDB = require("./db/connect");
@@ -15,13 +19,23 @@ const jobsRouter = require("./routes/jobs");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.set("trust proxy", 1); //enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx)
+// see https://expressjs.com/en/guide/behind-proxies.html
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  })
+);
 app.use(express.json());
+app.use(helmet());
 app.use(
   cors({
     origin: ["http://127.0.0.1:5173", "http://localhost:5173"],
     credentials: true,
   })
 );
+app.use(xss());
 // extra packages
 
 // routes
---
+++ documentation/README.md
@@ -0,0 +1,20 @@
+# Jobs API documentation
+
+## Usage
+
+- open the /redoc/redoc-static.html file in the browser
+
+## Installation instructions
+
+to build the redoc-static file for any API project:
+
+1. create a Postman collection of API requests
+2. export that collection as a JSON file
+3. go to https://p2o.defcon007.com/
+4. submit the exported JSON file
+5. copy the output code to a .yaml file, e.g. API-documentation.yaml
+6. copy the file to a documentation directory
+7. cd into that directory and run:
+   npx @redocly/cli build-docs API-documentation.yaml
+8. this should create redoc-static.html
+9. run redoc-static.html in the browser
