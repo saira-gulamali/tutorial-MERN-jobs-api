63768e77ecd973b80509038bf9f2ab99d00ba72d
saira-gulamali <sguacamole98@gmail.com>
2024-06-21 17:00:21 +0100

    move CORS config to .env file

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(helmet());
 app.use(
   cors({
-    origin: ["http://127.0.0.1:5173", "http://localhost:5173"],
+    origin: [process.env.CORS_PRD],
     credentials: true,
   })
 );
